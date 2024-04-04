f0780292309447ea87e3595443a3293b0bc92f9e
Evan Miller <emiller.arkko@gmail.com>
2024-04-04 22:35:47 +0300

    Rm more dead code

--- src/image_recognition/image_recognition_tools.clj
+++ src/image_recognition/image_recognition_tools.clj
@@ -1,27 +1,5 @@
 (ns image-recognition.image-recognition-tools
-  (:use mikera.image.core)
-  (:require [clojure.math :as math]))
-
-(defn- flatv [s]
-  (-> s flatten vec))
-
-(defn- rgb-difs [{ r1 :r g1 :g b1 :b} {r2 :r g2 :g b2 :b}]
-  {:r (abs (- r2 r1))
-   :g (abs (- g2 g1))
-   :b (abs (- b2 b1))})
-
-(defn- white-rgb? [{:keys [r g b]}]
-  (= 255 r g b))
-
-(defn- avg-rgb-dif [rgb1 rgb2]
-  (if
-   (or (white-rgb? rgb1)
-       (white-rgb? rgb2))
-    255
-    (let [{:keys [r g b]} (rgb-difs rgb1 rgb2)]
-      (-> (+ r g b)
-          (/ 3.0)))))
-
+  (:use mikera.image.core))
 
 (defn rgb-components
   "Return the RGB components of a colour value, in a 3-element map of long values"
@@ -37,59 +15,5 @@
 (defn img-wh [img]
   [(.getWidth img) (.getHeight img)])
 
-(defn- get-image-rgb-grid [img n]
-  (let [[w h] (img-wh img)
-        interval #(-> %
-                     (/ n)
-                     (math/floor))
-        ->coords-range #(-> (range 0 % (interval %))
-                     rest
-                     drop-last)
-        x-coords (->coords-range w)
-        y-coords (->coords-range h)]
-    (mapv (fn [x]
-            (mapv (fn [y]
-                    (.getRGB img x y))
-                  y-coords)) x-coords)))
 
-
-
-(defn grid-rgb-difs [img-1 img-2 n]
-  (let [grid-1-flat (flatv (get-image-rgb-grid img-1 n))
-        grid-2-flat (flatv (get-image-rgb-grid img-2 n))]
-    (->> (range (min (count grid-1-flat) (count grid-2-flat)))
-         (map
-          (fn [i]
-           (avg-rgb-dif  (get grid-1-flat i)  (get grid-2-flat i)))))))
-
-(defn matching-grid-pixel-count [img-1 img-2 n]
-  (let [rgb-diffs (grid-rgb-difs img-1 img-2 n)]
-    (->> rgb-diffs
-         (filter #(< % 8))
-         count)))
-
-(def placements-ascending [1 2 3 4 5 6 7 8 9 10 11 12])
-
-(defn- load-placement-images []
-  (mapv #(load-image-resource (str "images/placements/" % ".png")) placements-ascending))
-
-(defn load-test-placement [n]
-  (load-image-resource (str "images/placements/test/" n ".png")))
-
-(defn get-test-img-color-grid [name n]
-  (let [img (load-image-resource (str "images/test/" name ".png"))]
-    (get-image-rgb-grid img n)))
-
-(defn print-image-data []
-  (prn (str "images/" "1st" ".png"))
-  )
-
-(defn recognize-placement [path]
-  nil)
-
-(defn recognize-item-1 [path]
-  nil)
-
-(defn recognize-item-2 [path]
-  nil)
 
